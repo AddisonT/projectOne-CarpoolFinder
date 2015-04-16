e623f211682ec1dd5fb99199ee49832a9b932d03
AddisonT <addisontam1@Addisons-MacBook-Pro.local>
2015-04-16 13:49:49 -0700

    added some nav

--- models/user.js
+++ models/user.js
@@ -33,16 +33,45 @@ module.exports = function (sequelize, DataTypes){
 				var hash = bcrypt.hashSync(passwordD, salt);
 				return hash;
 			},
-			createSecure: function(email, passwordD) {
-				if(passwordD.length < 6) {
-					throw new Error("Password too short");
-				}
-				return this.create({
-					email: email,
-					password: this.encryptPassword(passwordD)
-				});
+			// createSecure: function(email, passwordD) {
+			// 	if(passwordD.length < 6) {
+			// 		throw new Error("Password too short");
+			// 	}
+			// 	return this.create({
+			// 		email: email,
+			// 		password: this.encryptPassword(passwordD)
+			// 	});
 
-			},
+			// },
+			createSecure: function(email, passwordD) {
+	        // console.log("hi " + (typeof this.findAndCountAll( { where: { email: email } })));
+	        // If password is too short, throw error
+	        if(passwordD.length < 6) {
+		          throw new Error("Password too short");
+		        } else {
+		          // Else, check if user already exists in db
+		          // Create variable for nested object User
+		        var _this = this
+		          // Return User db count where email is the same as email passed through function/entered into form
+		        return this.count( {where: { email: email } } )
+		            // Then run this function on userCount
+		            .then( function(userCount) {
+		              // Log count # in console
+		              console.log("count returned " + JSON.stringify(userCount));
+		              // Check if userCount is greater than 1 (aka check if email in db already)
+		              if (userCount >= 1) {
+		                // If true, throw error
+		                throw new Error("Email already exists");
+		              } else {
+		                // Else, instantiate new User! (Hooray!)
+		                console.log("WERE GETTING HERE\n\n\n\n\n");
+		                return _this.create({
+		                  email: email,
+		                  password: _this.encryptPassword(password)});
+		              }
+		            });
+		        }
+		      },
 			authenticate: function(email, passwordD) {
 				// find a user in the DB
 				return this.find({
